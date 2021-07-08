07004da1432178f4de7b2e83e9605e0a9fbc9d3c
Omar <xxxomarxxx21@gmail.om>
2021-07-08 06:15:37 +0200

    minor function change

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 
 
+def read():
+    return pd.read_csv(
+        'https://archive.ics.uci.edu/ml/machine-learning-databases/magic/magic04.data',
+        sep=',', header=None)
+
+
 def balance(instances):
     i = 5644  # 5644 is the number of classes (g) that should be randomly removed for the two classes to be balanced
     while i > 0:
@@ -29,6 +35,35 @@ def split(instances):
     return x, y, x_train, x_test, y_train, y_test
 
 
+def cal_accuracy(y_test, y_pred):
+    print("Confusion Matrix: ",
+          confusion_matrix(y_test, y_pred))
+
+    print("Accuracy: ",
+          accuracy_score(y_test, y_pred) * 100)
+
+    print("Report:",
+          classification_report(y_test, y_pred))
+
+
+def prediction(X_test, clf_object):
+    y_pred = clf_object.predict(X_test)
+    print("Predicted values:")
+    print(y_pred)
+    return y_pred
+
+
+def decision_tree(x_train, x_test, y_train, y_test):
+    clf_gini = training_with_gini(x_train, y_train)
+    clf_entropy = training_with_entropy(x_train, y_train)
+    y_pred_gini = prediction(x_test, clf_gini)
+    print("******Decision Tree: Gini******")
+    cal_accuracy(y_test, y_pred_gini)
+    y_pred_entropy = prediction(x_test, clf_entropy)
+    print("******Decision Tree: Entropy******")
+    cal_accuracy(y_test, y_pred_entropy)
+
+
 def training_with_gini(x_train, y_train):
     clf_gini = DecisionTreeClassifier(criterion="gini", random_state=100)
     clf_gini.fit(x_train, y_train)
@@ -41,33 +76,22 @@ def training_with_entropy(x_train, y_train):
     return clf_entropy
 
 
-def prediction(X_test, clf_object):
-    y_pred = clf_object.predict(X_test)
-    print("Predicted values:")
-    print(y_pred)
-    return y_pred
-
-
-def cal_accuracy(y_test, y_pred):
-    print("Confusion Matrix: ",
-          confusion_matrix(y_test, y_pred))
-
-    print("Accuracy: ",
-          accuracy_score(y_test, y_pred) * 100)
-
-    print("Report:",
-          classification_report(y_test, y_pred))
-
-
-def decision_tree(x_train, x_test, y_train, y_test):
-    clf_gini = training_with_gini(x_train, y_train)
-    clf_entropy = training_with_entropy(x_train, y_train)
-    y_pred_gini = prediction(x_test, clf_gini)
-    print("******Decision Tree: Gini******")
-    cal_accuracy(y_test, y_pred_gini)
-    y_pred_entropy = prediction(x_test, clf_entropy)
-    print("******Decision Tree: Entropy******")
-    cal_accuracy(y_test, y_pred_entropy)
+def adaboost(x_train, x_test, y_train, y_test):
+    scores_list = []
+    n_range = range(1, 75)
+    print("******AdaBoost******")
+    for n in n_range:
+        abc = AdaBoostClassifier(n_estimators=n)
+        abc.fit(x_train, y_train)
+        y_pred = abc.predict(x_test)
+        scores_list.append(metrics.accuracy_score(y_test, y_pred))
+        print("Accuracy Report for n=", n)
+        cal_accuracy(y_test, y_pred)
+    plt.plot(n_range, scores_list)
+    plt.title('Adaboost with n_estimators tuned')
+    plt.xlabel("Value of N")
+    plt.ylabel("Testing Accuracy")
+    plt.show()
 
 
 def knn(x_train, x_test, y_train, y_test):
@@ -89,14 +113,6 @@ def knn(x_train, x_test, y_train, y_test):
     plt.show()
 
 
-def naive_bayes(x_train, x_test, y_train, y_test):
-    gnb = GaussianNB()
-    gnb.fit(x_train, y_train)
-    y_pred = gnb.predict(x_test)
-    print("******Naive Bayes******")
-    cal_accuracy(y_test, y_pred)
-
-
 def random_forests(x_train, x_test, y_train, y_test):
     scores_list = []
     n_range = range(1, 75)
@@ -115,28 +131,16 @@ def random_forests(x_train, x_test, y_train, y_test):
     plt.show()
 
 
-def adaboost(x_train, x_test, y_train, y_test):
-    scores_list = []
-    n_range = range(1, 75)
-    print("******AdaBoost******")
-    for n in n_range:
-        abc = AdaBoostClassifier(n_estimators=n)
-        abc.fit(x_train, y_train)
-        y_pred = abc.predict(x_test)
-        scores_list.append(metrics.accuracy_score(y_test, y_pred))
-        print("Accuracy Report for n=", n)
-        cal_accuracy(y_test, y_pred)
-    plt.plot(n_range, scores_list)
-    plt.title('Adaboost with n_estimators tuned')
-    plt.xlabel("Value of N")
-    plt.ylabel("Testing Accuracy")
-    plt.show()
+def naive_bayes(x_train, x_test, y_train, y_test):
+    gnb = GaussianNB()
+    gnb.fit(x_train, y_train)
+    y_pred = gnb.predict(x_test)
+    print("******Naive Bayes******")
+    cal_accuracy(y_test, y_pred)
 
 
 if __name__ == "__main__":
-    instances = pd.read_csv(
-        'https://archive.ics.uci.edu/ml/machine-learning-databases/magic/magic04.data',
-        sep=',', header=None)
+    instances = read()
     instances = balance(instances)
     x, y, x_train, x_test, y_train, y_test = split(instances)
     classifications = [decision_tree, knn, naive_bayes, random_forests, adaboost]
