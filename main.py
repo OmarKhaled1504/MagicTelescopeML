53362b3d4193aefdc5987ad0b3d5ed8753d99c9d
youssefdarwish37 <85325600+youssefdarwish37@users.noreply.github.com>
2021-07-10 16:47:04 +0200

    spaces

--- main.py
+++ main.py
@@ -38,13 +38,13 @@ def split(instances):
 
 
 def cal_accuracy(y_test, y_pred):
-    print("Confusion Matrix: ",
+    print("Confusion Matrix: \n",
           confusion_matrix(y_test, y_pred))
 
     print("Accuracy: ",
           accuracy_score(y_test, y_pred) * 100)
 
-    print("Report:",
+    print("Report:\n",
           classification_report(y_test, y_pred))
 
 
@@ -62,12 +62,12 @@ def decision_tree(x_train, x_test, y_train, y_test):
     clf_gini = training_with_gini(x_train, y_train)
     clf_entropy = training_with_entropy(x_train, y_train)
     y_pred_gini = clf_gini.predict(x_test)
-    print("******Decision Tree: Gini******")
+    print("********** Decision Tree: Gini **********")
     max_accuracy.append(accuracy_score(y_test, y_pred_gini) * 100)
     cal_accuracy(y_test, y_pred_gini)
     y_pred_entropy = clf_entropy.predict(x_test)
     max_accuracy.append(accuracy_score(y_test, y_pred_entropy) * 100)
-    print("******Decision Tree: Entropy******")
+    print("********** Decision Tree: Entropy **********")
     cal_accuracy(y_test, y_pred_entropy)
 
 
@@ -86,7 +86,7 @@ def training_with_entropy(x_train, y_train):
 def adaboost(x_train, x_test, y_train, y_test):
     scores_list = []
     n_range = range(1, 75)
-    print("******AdaBoost******")
+    print("********** AdaBoost **********")
     for n in n_range:
         abc = AdaBoostClassifier(n_estimators=n)
         abc.fit(x_train, y_train)
@@ -94,6 +94,7 @@ def adaboost(x_train, x_test, y_train, y_test):
         scores_list.append(metrics.accuracy_score(y_test, y_pred))
         print("Accuracy Report for n=", n)
         cal_accuracy(y_test, y_pred)
+        print("********************************")
     print("Most accurate n: {}, with Accuracy {}".format(scores_list.index(max(scores_list)) - 1,
                                                          scores_list[scores_list.index(max(scores_list))] * 100))
     max_accuracy.append(max(scores_list) * 100)
@@ -108,7 +109,7 @@ def adaboost(x_train, x_test, y_train, y_test):
 def knn(x_train, x_test, y_train, y_test):
     scores_list = []
     k_range = range(1, 50)
-    print("******KNN******")
+    print("********** KNN **********")
     for k in k_range:
         knn = KNeighborsClassifier(n_neighbors=k)
         knn.fit(x_train, y_train)
@@ -116,6 +117,7 @@ def knn(x_train, x_test, y_train, y_test):
         scores_list.append(metrics.accuracy_score(y_test, y_pred))
         print("Accuracy Report for k=", k)
         cal_accuracy(y_test, y_pred)
+        print("********************************")
     print("Most accurate k: {}, with Accuracy {}".format(scores_list.index(max(scores_list)) - 1,
                                                          scores_list[scores_list.index(max(scores_list))] * 100))
     max_accuracy.append(max(scores_list) * 100)
@@ -130,7 +132,7 @@ def knn(x_train, x_test, y_train, y_test):
 def random_forests(x_train, x_test, y_train, y_test):
     scores_list = []
     n_range = range(1, 75)
-    print("******Random Forests******")
+    print("********** Random Forests **********")
     for n in n_range:
         rfc = RandomForestClassifier(n_estimators=n)
         rfc.fit(x_train, y_train)
@@ -138,6 +140,7 @@ def random_forests(x_train, x_test, y_train, y_test):
         scores_list.append(metrics.accuracy_score(y_test, y_pred))
         print("Accuracy Report for n=", n)
         cal_accuracy(y_test, y_pred)
+        print("********************************")
     print("Most accurate n: {}, with Accuracy {}".format(scores_list.index(max(scores_list)) - 1,
                                                          scores_list[scores_list.index(max(scores_list))] * 100))
     max_accuracy.append(max(scores_list) * 100)
@@ -153,7 +156,7 @@ def naive_bayes(x_train, x_test, y_train, y_test):
     gnb = GaussianNB()
     gnb.fit(x_train, y_train)
     y_pred = gnb.predict(x_test)
-    print("******Naive Bayes******")
+    print("********** Naive Bayes **********")
     cal_accuracy(y_test, y_pred)
     max_accuracy.append(accuracy_score(y_test, y_pred) * 100)
 
